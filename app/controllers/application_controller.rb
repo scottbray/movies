cc1be71fcde5731076c28d427dc8eb50f90e44c3
Scott Bray <scott.bray@mckesson.com>
2017-09-12 20:29:31 +0000

    add host variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :gethost
+  
+  def hethost
+    @hostname = ENV['HOSTNAME'] || 'Anonymous'
+  end
+  
 end
