1661718e0243c1007729a59edd31a8bdbe9bc2e6
Scott Bray <scott.bray@mckesson.com>
2017-09-12 20:55:34 +0000

    changed controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :gethost
   
-  def hethost
+  def gethost
     @hostname = ENV['HOSTNAME'] || 'Anonymous'
   end
   
